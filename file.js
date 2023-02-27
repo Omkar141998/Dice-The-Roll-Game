d80c3016933ad5e49396051bf8f601981eb10dea
Omkar141998 <onagpure688@gmail.com>
2023-02-27 21:24:55 +0530

    hnage Function

--- file.js
+++ file.js
@@ -1,58 +1,35 @@
-let playerName = document.querySelectorAll("input")
-let btns = document.getElementsByClassName("Diceroll")
-let scores = document.getElementsByTagName("span")
+let playerNames = document.querySelectorAll("input");
+let btns = document.getElementsByClassName("Diceroll");
+let scores = document.getElementsByTagName("span");
 
-//[b1, b2, b3, b4, b5, b6]
-//event, addEvent Listner
+//[b1,b2,b3,b4,b5,b6]
+//Event AddEvent Listener
 
-for(let i=0;i<=btns.length-1;i++){
-     btns[i].addEventListener("click", ()=>{rollDice(btns[i].id)})
+for(let i=0; i<= btns.length-1;i++){
+  btns[i].addEventListener("click", ()=>{rollTheDice(btns[i].id)})
 }
-function rollDice(btn_id){
+function rollTheDice(btn_id){
+  btns[btn_id].disabled = true;
 
-    btns[btn_id].disabled = true
-    
-    let  arr= ["1", "2", "3", "4", "5", "6"]
-    // 0 - 5
-    let randomIndex = Math.floor(Math.random() * 6) // 0 - 5 only integers
-    let randomDice = arr[randomIndex]
-    // console.log(randomDice)
-    scores[btn_id].innerText = randomDice
+  let arr = ['1','2','3','4','5','6']
+  //0 - 5
+
+  // let randomIndex = Math.floor(Math.random() * 6)
+    // o -5 Only Integers
+
+  // let randomDice = arr[randomIndex];
+  // //console.log(randomDice);
+
+  // scores[btn_id].innerText = randomDice;
+  let randomIndex = Math.floor(Math.random() * arr.length);// 0 - 6; only integers
+
+  let randomDice = arr[randomIndex];
+  // console.log(randomDice);
+  scores[btn_id].innerText = randomDice;
   
-  }
-
- 
-
-//  let playerName = document.querySelectorAll("input")
-// let btns = document.getElementsByClassName("diceRollButton")
-// let scores = document.getElementsByTagName("span")
-
-// // [b1,b2,n3,b4,b5,b6]
-
-// // events,  addEventListener
 
 
-
-// for(let i = 0; i<=btns.length-1; i++ ){
-//    btns[i].addEventListener("click", ()=>{rollDice(btns[i].id)})
-// }
-
-
-// function rollDice(btn_id){
-
-//    btns[btn_id].disabled = true
-   
-//    let  arr= ["1", "2", "3", "4", "5", "6"]
-//    // 0 - 5
-//    let randomIndex = Math.floor(Math.random() * 6) // 0 - 5 only integers
-//    let randomDice = arr[randomIndex]
-//    // console.log(randomDice)
-//    scores[btn_id].innerText = randomDice
- 
-// }
-
-
-// random => 0<=x<1
+}
 
 
 
